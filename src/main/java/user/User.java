08c999c498baafe753bda4048990027f29e164f2
Helmersson <50464822+helmersson@users.noreply.github.com>
2025-03-31 15:25:36 +0200

    as

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,27 @@
-package userTest;
+package user;
 
 public class User {
+    private String userName;
+    private String passWord;
+
+    public User(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setUserName(String newUserName) {
+        userName = newUserName;
+    }
+
+    public void setPassWord(String newPassWord) {
+        passWord = newPassWord;
+    }
 }
--- src/test/java/userTest/UserTest.java
+++ src/test/java/userTest/UserTest.java
@@ -1,13 +1,48 @@
+package userTest;
+
 import org.junit.jupiter.api.Test;
+import user.User;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserTest {
     @Test
     public void testCreateUser(){
-        User newUser = new User ();
+        User user = new User ("Wille", "password123");
+    }
 
-        String expected = "";
-        String actual = newUser.getUsername;
+    @Test
+    public void testGetUserName(){
+        User user = new User ("Wille", "password123");
+        String expected = "Wille";
+        String actual = user.getUsername();
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testGetPassWord(){
+        User user = new User ("Wille", "password123");
+        String expected = "password123";
+        String actual = user.getPassWord();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetUserName(){
+        User user = new User ("Wille", "password123");
+        String expected = "Henry";
+        user.setUserName("Henry");
+        String actual = user.getUsername();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetPassWord(){
+        User user = new User ("Wille", "password123");
+        String expected = "password!";
+        user.setPassWord("password!");
+        String actual = user.getPassWord();
+        assertEquals(expected, actual);
     }
 
 }
